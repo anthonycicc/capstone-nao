51b972ea879ddde93010e14b04cf4d556bf342e0
Aslatts <adam.slattery.512@gmail.com>
2016-10-23 19:55:27 -0400

    Update UI model.py

--- TCL/UI model.py
+++ TCL/UI model.py
@@ -1,65 +1,62 @@
+# Imports and preprocessor
 from tkinter import *
 from tkinter import ttk
 root = Tk()
-root.title("Function scrollbox")
+root.title("Nao Programming IDE")
 
+# Function definitions. Currently This is only a placehoder (this is what the top menu bar is currently calling)
 def hello():
     print("hello!")
 
-menubar = Menu(root)
-
+# Top level menu
 # create the file menu
+menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="Open", command=hello)
 filemenu.add_command(label="Save", command=hello)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
-
 # create the edit menu
 editmenu = Menu(menubar, tearoff=0)
 editmenu.add_command(label="Cut", command=hello)
 editmenu.add_command(label="Copy", command=hello)
 editmenu.add_command(label="Paste", command=hello)
 menubar.add_cascade(label="Edit", menu=editmenu)
-
 #create the Run menu
 editmenu = Menu(menubar, tearoff=0)
 editmenu.add_command(label="Compile", command=hello)
 editmenu.add_command(label="Run", command=hello)
 editmenu.add_command(label="Debug", command=hello)
 menubar.add_cascade(label="Run", menu=editmenu)
-
 #Create the help menu
 helpmenu = Menu(menubar, tearoff=0)
 helpmenu.add_command(label="About", command=hello)
 menubar.add_cascade(label="Help", menu=helpmenu)
-
 # display the menu
 root.config(menu=menubar)
 
-# Initialize our country "databases":
-#  - the list of country codes (a subset anyway)
-#  - a parallel list of country names, in the same order as the country codes
-#  - a hash table mapping country code to population<
+
+#Hash table of function names
 functionCodes = ('wf', 'wb', 'tr', 'tl', 'ki', 'li', 'sy')
 functionNames = ('WalkForwards', 'WalkBackwards', 'TurnRight', 'TurnLeft', 'Kick', 'Lift', 'Say')
 fnames = StringVar(value=functionNames)
 descriptions = {'wf':'move the robot forwards', 'wb':'move the robot backwards', 'tr':'turn the robot right', 'tl':'turn the robot left','ki':'make the robot kick', 'li':'make the robot lift its arms', 'sy':'make the robot speak'}
 
 
-# State variables
-statusmsg = StringVar()
+# String variables
+functionDescription = StringVar()
+search = StringVar()
 
 # Called when the selection in the listbox changes
 def showDescriptions(*args):
-    idxs = lbox.curselection()
+    idxs = function_list.curselection()
     if len(idxs)==1:
         idx = int(idxs[0])
         code = functionCodes[idx]
         name = functionNames[idx]
         popn = descriptions[code]
-        statusmsg.set("The funciton %s will %s" % (name, popn))
+        functionDescription.set("The funciton %s will %s" % (name, popn))
 
 
 
@@ -70,27 +67,40 @@ c.grid_columnconfigure(0, weight=1)
 c.grid_rowconfigure(0,weight=1)
 
 # Create the different widgets
-lbox = Listbox(c, listvariable=fnames, height=5)
-scrollbar = ttk.Scrollbar(c, orient=VERTICAL, command=lbox.yview)
-lbox.configure(yscrollcommand=scrollbar.set)
-status = ttk.Label(c, textvariable=statusmsg, anchor=W)
+function_list = Listbox(c, listvariable=fnames, height=5)
+function_scrollbar = ttk.Scrollbar(c, orient=VERTICAL, command=function_list.yview)
+function_list.configure(yscrollcommand=function_scrollbar.set)
+function_desc = ttk.Label(c, textvariable=statusmsg, anchor=W, width=50)
+frame = ttk.Frame(c, borderwidth=1, height=800, width=800, relief=SUNKEN)
+search_entry = ttk.Entry(c, width=20, textvariable=search,)
+search_button = ttk.Button(c, text="Search", command=showDescriptions(),)
+
 
 # Grid all the widgets
-lbox.grid(column=0, row=0, sticky=(N,S,E,W))
-status.grid(column=0, row=2, columnspan=2, sticky=(W,E))
-scrollbar.grid(column=2, row=0, sticky=(N,S))
+#Col 1
+search_entry.grid(column=1, row=1)
+search_button.grid(column=1, row=2)
+function_list.grid(column=1, row=3, sticky=(N,S,E,W))
+function_desc.grid(column=1, row=4, sticky=(W,E))
+
+#Col 2
+function_scrollbar.grid(column=2, row=3, sticky=(N,S))
+
+#Col 3
+frame.grid(column=3, row=3, sticky=(N,S,E,W))
+
 
 
 # Set event bindings for when the selection in the listbox changes,
-lbox.bind('<<ListboxSelect>>', showDescriptions)
+function_list.bind('<<ListboxSelect>>', showDescriptions)
 
-# Colorize alternating lines of the listbox
+# Color alternating lines of the listbox
 for i in range(0,len(functionNames),2):
-    lbox.itemconfigure(i, background='#f0f0ff')
+    function_list.itemconfigure(i, background='#f0f0ff')
 
 # Set the starting state of the interface
-statusmsg.set('')
-lbox.selection_set(0)
+functionDescription.set('')
+function_list.selection_set(0)
 showDescriptions()
 
 root.mainloop()
