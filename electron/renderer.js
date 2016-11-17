7c97674f8603d05fc360df2734c1322592169525
Skye <skye@skye.moe>
2016-11-16 20:03:58 -0500

    Only use our beginner blocks, just for fun

--- electron/renderer.js
+++ electron/renderer.js
@@ -9,18 +9,48 @@ const {dialog} = require('electron').remote;
 var workspace;
 // start up blockly
 document.addEventListener("DOMContentLoaded", () => {
+  blocks.map(block => addBlock(block));
+
   workspace = Blockly.inject('blocklyDiv', {
-    toolbox: document.getElementById('toolbox')
+    toolbox: makeToolBox(blocks)
   });
 
-  document.getElementById("save-button").addEventListener("click", saveFile);
-  document.getElementById("load-button").addEventListener("click", loadFile);
-  document.getElementById("run-button").addEventListener("click", executeCode);
+  document.getElementById("save-button")
+    .addEventListener("click", saveFile);
+  document.getElementById("load-button")
+    .addEventListener("click", loadFile);
+  document.getElementById("run-button")
+    .addEventListener("click", executeCode);
 });
 
+function addBlock(block) {
+  Blockly.Blocks[block.type] = {
+    init: function() {
+      this.jsonInit(block);
+    }
+  };
+}
+
+const makeToolBox = blockList => {
+  const start =
+        `<xml id="toolbox" style="display: none">`;
+  const logical_stuff = `
+    <block type="controls_if"></block>
+    <block type="controls_repeat_ext"></block>
+    <block type="logic_compare"></block>
+    <block type="math_number"></block>
+    <block type="math_arithmetic"></block>
+    <block type="text"></block>
+    <block type="text_print"></block>`;
+
+  const blocks_xml = blockList.map(block => `<block type="${block.type}"></block>`).join("\n");
+  console.log(blocks_xml);
+
+  return start + blocks_xml + `</xml>`;
+};
 
 // Save to file
-function saveFile(){
+function saveFile() {
   const xml = Blockly.Xml.workspaceToDom(workspace);
   const xml_text = Blockly.Xml.domToText(xml);
 
@@ -28,8 +58,11 @@ function saveFile(){
   fs.writeFileSync(filePath, xml_text);
   console.log('It\'s Saved');
 }
+
 function loadFile() {
-  const filePaths = dialog.showOpenDialog({properties: ['openFile']});
+  const filePaths = dialog.showOpenDialog({
+    properties: ['openFile']
+  });
   const xml_text = fs.readFileSync(filePaths[0]);
   workspace.clear();
   const xml = Blockly.Xml.textToDom(xml_text);
@@ -43,5 +76,3 @@ function executeCode() {
 
   run_code = child_process.spawn('python' ['tmp.py']);
 }
-
-
